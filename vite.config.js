bb8cbf1d60b742937178a064c6a4aa6f9b62dd44
enpei.chen <chenenpei93@gmail.com>
2022-05-28 11:09:13 +0800

    Update packge.json and vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -13,6 +15,7 @@ export default defineConfig({
     Components({
       resolvers: [AntDesignVueResolver()],
     }),
+    VitePWA({})
   ],
   base: "/vite-iptv-app/",
 });
