f9bb1f4be04e12ea92f41b8e10a746281d29d3a2
wu-honghao <2698981813@qq.com>
2022-09-13 19:14:39 +0800

    update: vite.config.js & package.json

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,46 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
-import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [AntDesignVueResolver()],
-    }),
-    Components({
-      resolvers: [AntDesignVueResolver()],
-    }),
-    VitePWA({
-      manifest: {
-        name: "iptv",
-        short_name: "iptv",
-        description:
-          "Application to subscribe to global network TV channels at any time",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "./icons/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "./icons/android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ],
-  base: "/vite-iptv-app/",
-  build: {
-    manifest: true,
-    rollupOptions: {
-      // 覆盖默认的 .html 入口
-      input: "index.html",
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [AntDesignVueResolver()],
+        }),
+        Components({
+            resolvers: [AntDesignVueResolver()],
+        }),
+        VitePWA({
+            manifest: {
+                name: 'iptv',
+                short_name: 'iptv',
+                description: 'Application to subscribe to global network TV channels at any time',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: './icons/android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: './icons/android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
+    ],
+    base: '/',
+    build: {
+        manifest: true,
+        rollupOptions: {
+            // 覆盖默认的 .html 入口
+            input: 'index.html',
+        },
     },
-  },
 });
